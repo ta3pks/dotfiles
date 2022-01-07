aa955d91855572cbfb75c490bf313074947be79b
nikos efthias <nikos@mugsoft.io>
2022-01-07 13:18:54 +0300

    dont use new fancy tabs

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -6,6 +6,9 @@ return {
 	send_composed_key_when_left_alt_is_pressed=false,
 	send_composed_key_when_right_alt_is_pressed=true,
 	native_macos_fullscreen_mode=true,
+	hide_tab_bar_if_only_one_tab=true,
+	use_fancy_tab_bar=false,
+	tab_bar_at_bottom=true,
 	keys = {
 		{key="e",mods="SUPER",action=wezterm.action{SplitHorizontal={}}},
 		{key="e",mods="SUPER|CTRL",action=wezterm.action{SplitVertical={}}},

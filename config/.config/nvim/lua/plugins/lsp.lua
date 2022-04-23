d386f1f4a3daf0c3e6bad4a2884b0038573e279a
nikos efthias <nikos@mugsoft.io>
2022-04-23 17:43:46 +0300

    autofmt lua

--- config/.config/nvim/lua/plugins/lsp.lua
+++ config/.config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,24 @@
 vim.cmd [[
 augroup __AutoFormatters
 	au! __AutoFormatters
-	au BufWritePre *.js,*.ts,*.rs lua vim.lsp.buf.formatting_sync()
+	au BufWritePre *.js,*.ts,*.rs,*.lua lua vim.lsp.buf.formatting_sync()
 augroup END
 ]]
 local keymaps = require("keymaps")
 
+
 -- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
 -- or if the server is already installed).
 keymaps.n({
-    ["<c-k>"] = ":lua vim.diagnostic.goto_prev()<cr>",
-    ["<c-j>"] = ":lua vim.diagnostic.goto_next()<cr>",
-  })
+  ["<c-k>"] = ":lua vim.diagnostic.goto_prev()<cr>",
+  ["<c-j>"] = ":lua vim.diagnostic.goto_next()<cr>",
+})
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  keymaps.n{
+  keymaps.n {
     ["<c-]>"] = ":Telescope lsp_definitions<cr>",
-    ["K"] = {":lua vim.lsp.buf.hover()<cr>"},
+    ["K"] = { ":lua vim.lsp.buf.hover()<cr>" },
     ["<leader>lr"] = ":Telescope lsp_references<cr>",
     ["<leader>q"] = ":Telescope lsp_range_code_actions<cr>",
     ["<m-s>"] = ":Telescope lsp_workspace_symbols<cr>",
@@ -27,47 +28,52 @@ local on_attach = function(client, bufnr)
     ["<m-c-l>"] = ":lua vim.lsp.buf.formatting()<cr>",
   }
 end
-local servers = {'rust_analyzer'}
+local servers = { 'rust_analyzer' }
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local ra_config = { --{{{
+  enable = true,
+  rustfmt = {
+    extraArgs = { "+nightly" }
+  },
+  cargo = {
+    allFeatures = false,
+    features = {}
+  },
+  checkOnSave = {
+    command = "check",
+    enable = true,
+    allFeatures = false,
+  },
+  diagnostics = {
+    disabled = {
+      "unresolved-proc-macro",
+      "unresolved-macro-call",
+      "macro-error",
+      "inactive-code"
+    }
+  },
+  inlayHints = {
+    enable = true
+  },
+  completion = {
+    postfix = { enable = true }
+  }
+} --}}}
 require("nvim-lsp-installer").on_server_ready(function(server)
-  server:setup {
+  local cfg = {
     capabilities = capabilities,
     on_attach = on_attach,
     flags = {
       -- This will be the default in neovim 0.7+
       debounce_text_changes = 150,
     },
-    settings={
-      ['rust-analyzer'] = {
-        enable=true,
-        rustfmt = {
-          extraArgs = { "+nightly" }
-        },
-        cargo ={
-          allFeatures = false,
-          features = {}
-        },
-        checkOnSave={
-          command = "check",
-          enable = true,
-          allFeatures = false,
-        },
-        diagnostics = {
-          disabled = {
-            "unresolved-proc-macro",
-            "unresolved-macro-call",
-            "macro-error",
-            "inactive-code"
-          }
-        },
-        inlayHints = {
-          enable=true
-        },
-        completion = {
-          postfix = {enable=true}
-        }
-      }
-    }
   }
-
+  if server.name == "rust_analyzer" then
+    cfg.settings = { ['rust-analyzer'] = ra_config }
+  elseif server.name == "denols" then
+    cfg.init_options = {
+      lint = true,
+    }
+  end
+  server:setup(cfg)
 end)

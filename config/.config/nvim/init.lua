9f3a53b5eae641c643fb72380f22e15910f98cf8
nikos efthias <nikos@mugsoft.io>
2022-12-26 18:32:02 +0300

    updates

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -1,10 +1,13 @@
 require("events")
 require("keymaps")
 require("plugins")
-local api = vim.api
+-- local api = vim.api
 vim.cmd([[
 	syntax on
 	command! FixTrailing :%s/\s\+$//g
+	"osx specific settings
+	inoremap <d-v> <c-r>+
+	vnoremap <d-c> "+ygv
 ]])
 vim.o.inccommand = "split"
 vim.o.ignorecase = true
@@ -20,8 +23,8 @@ vim.o.foldmethod = "marker"
 vim.o.termguicolors = true
 vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
 vim.o.mouse = 'a'
-vim.o.guifont = "FiraCode Nerd Font:h14"
 if vim.fn.exists("g:neovide") then
+	vim.o.guifont = "FiraCode Nerd Font:h14"
 	vim.g.neovide_hide_mouse_when_typing = true
 	vim.g.neovide_full_screen = true
 	vim.g.neovide_macos_option_as_alt = true
@@ -29,4 +32,7 @@ if vim.fn.exists("g:neovide") then
 	vim.g.neovide_cursor_animation_length = 0.05
 	vim.g.neovide_cursor_trail_size = 0.1
 	vim.g.neovide_cursor_vfx_mode = ""
+	vim.g.neovide_no_idle = false
+	vim.g.neovide_refresh_rate_idle = 1
+	vim.g.neovide_transparency = 0.9
 end

badd6fd19b6c2af722666beefaee06970a296f66
nikos efthias <nikos@mugsoft.io>
2023-09-23 02:23:23 +0300

    openterm now supports tmux

--- config/.config/nvim/lua/plugins/openterm.lua
+++ config/.config/nvim/lua/plugins/openterm.lua
@@ -1,28 +1,46 @@
-local m = {}
-local function do_open_term(prg)
-	if prg then
-		vim.cmd('term ' .. prg)
-		vim.cmd('normal! i')
-	else
-		vim.cmd [[term
-		normal! i
-		]]
-	end
+local function is_tmux()
+	return vim.fn.exists('$TMUX') == 1
 end
+local function tmux_open_term_full(prg)
+	prg = prg or ""
+	vim.cmd('silent !tmux new-window ' .. prg)
+end
+local function tmux_open_term_split(down, prg)
+	local cmd = "split -h"
+	prg = prg or ""
+	if down then
+		cmd = "split -v"
+	end
+	vim.cmd('silent !tmux ' .. cmd .. ' ' .. prg)
+end
+
+local function open_nvim_term(prg)
+	prg = prg or ""
+	vim.cmd('term' .. prg)
+	vim.cmd('normal! i')
+end
+
+local m = {}
 function m.open_full_term(prg)
-	vim.cmd('tabnew')
-	do_open_term(prg)
+	if is_tmux() then
+		tmux_open_term_full(prg)
+	else
+		vim.cmd('tabnew')
+		open_nvim_term(prg)
+	end
 end
 
 function m.open_term(down, prg)
-	local cmd
-	if down then
-		cmd = "sp"
+	if is_tmux() then
+		tmux_open_term_split(down, prg)
 	else
-		cmd = "vsp"
+		local cmd = "vsp"
+		if down then
+			cmd = "sp"
+		end
+		vim.cmd(cmd)
+		open_nvim_term(prg)
 	end
-	vim.cmd(cmd)
-	do_open_term(prg)
 end
 
 return m

2e5037e3e234ba23dde6b064ebd13d206381da08
nikos efthias <nikos@mugsoft.io>
2024-01-20 18:36:37 +0300

    kitty update

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -13,7 +13,8 @@ return {
 		config = function()
 			vim.cmd [[
 			let g:everforest_enable_italic = 1
-			let g:everforest_background = 'soft'
+			"let g:everforest_background = 'soft'
+			let g:everforest_background = 'hard'
 			let g:everforest_sign_column_background = 'grey'
 			let g:everforest_ui_contrast = 'high'
 			colorscheme everforest

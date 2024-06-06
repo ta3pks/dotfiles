94b3f4567ce964742c3f334a775650eed70558fd
nikos efthias <nikos@mugsoft.io>
2024-06-07 00:18:21 +0300

    better layer switching

--- nvim/.config/nvim/lua/plugins/coc.lua
+++ nvim/.config/nvim/lua/plugins/coc.lua
@@ -1,62 +1,62 @@
 return {
-	'neoclide/coc.nvim',
-	branch = 'release',
-	init = function()
-		vim.keymap.set("n", "<leader>o", "<cmd>CocOutline<CR>", { noremap = true, silent = true })
-		vim.o.tagfunc = "CocTagFunc"
-		-- vim.keymap.set("n", "<leader><leader>r", "<cmd>CocRestart<CR>", { noremap = true, silent = true })
-		vim.keymap.set("n", "<leader>r", "<Plug>(coc-rename)", { noremap = true, silent = true })
-		vim.keymap.set("n", "<a-,>", "<Plug>(coc-fix-current)", { noremap = true, silent = true })
-		vim.keymap.set("n", "<a-.>", "<Plug>(coc-codeaction-cursor)", { noremap = true, silent = true })
-		vim.keymap.set("i", "<a-.>", function() vim.fn.CocAction('showSignatureHelp') end,
-			{ noremap = true, silent = true, })
-		vim.keymap.set("v", "<a-.>", "<Plug>(coc-codeaction-selected)", { noremap = true, silent = true })
-		vim.keymap.set("n", "<a-r>", "<Plug>(coc-codeaction-refactor)", { noremap = true, silent = true })
-		vim.keymap.set("n", "<m-s>", "<Plug>(coc-codeaction-source)", { noremap = true, silent = true })
-		vim.keymap.set("n", "<a-d>", ":CocList diagnostics<CR>", { noremap = true, silent = true })
-		vim.keymap.set("i", "<cr>", function()
-			if vim.fn["coc#pum#visible"]() == 1 then
-				return vim.fn["coc#_select_confirm"]()
-			else
-				return "<cr>"
-			end
-		end, { noremap = true, expr = true })
+    'neoclide/coc.nvim',
+    branch = 'release',
+    init = function()
+        vim.keymap.set("n", "<leader>o", "<cmd>CocOutline<CR>", { noremap = true, silent = true })
+        vim.o.tagfunc = "CocTagFunc"
+        -- vim.keymap.set("n", "<leader><leader>r", "<cmd>CocRestart<CR>", { noremap = true, silent = true })
+        vim.keymap.set("n", "<leader>r", "<Plug>(coc-rename)", { noremap = true, silent = true })
+        vim.keymap.set("n", "<a-,>", "<Plug>(coc-fix-current)", { noremap = true, silent = true })
+        vim.keymap.set("n", "<c-.>", "<Plug>(coc-codeaction-cursor)", { noremap = true, silent = true })
+        vim.keymap.set("i", "<c-.>", function() vim.fn.CocAction('showSignatureHelp') end,
+            { noremap = true, silent = true, })
+        vim.keymap.set("v", "<c-.>", "<Plug>(coc-codeaction-selected)", { noremap = true, silent = true })
+        vim.keymap.set("n", "<a-r>", "<Plug>(coc-codeaction-refactor)", { noremap = true, silent = true })
+        vim.keymap.set("n", "<m-s>", "<Plug>(coc-codeaction-source)", { noremap = true, silent = true })
+        vim.keymap.set("n", "<a-d>", ":CocList diagnostics<CR>", { noremap = true, silent = true })
+        vim.keymap.set("i", "<cr>", function()
+            if vim.fn["coc#pum#visible"]() == 1 then
+                return vim.fn["coc#_select_confirm"]()
+            else
+                return "<cr>"
+            end
+        end, { noremap = true, expr = true })
 
-		vim.keymap.set("n", "<c-j>", "<plug>(coc-diagnostic-next)", { noremap = true, silent = true })
-		vim.keymap.set("n", "<c-k>", "<plug>(coc-diagnostic-prev)", { noremap = true, silent = true })
-		vim.keymap.set("n", "gt", "<plug>(coc-type-definition)", { noremap = true, silent = true })
-		vim.keymap.set("n", "gd", "<plug>(coc-definition)", { noremap = true, silent = true })
-		vim.keymap.set("i", "<c-space>", "coc#refresh()", { noremap = true, silent = true, expr = true })
-		vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", { noremap = true, silent = true })
-		vim.keymap.set("n", "gr", "<Plug>(coc-references)", { noremap = true, silent = true })
-		vim.keymap.set({ "i", "n" }, "<c-c>", function()
-			if vim.fn["coc#pum#visible"]() == 1 then
-				return vim.fn["coc#pum#cancel"]()
-			elseif vim.fn["coc#float#has_float"]() == 1 then
-				return vim.fn["coc#float#close_all"]()
-			else
-				return "<c-c>"
-			end
-		end, { noremap = true, silent = true, expr = true })
-		local function ShowDocumentation()
-			if vim.fn.CocAction("hasProvider", "hover") then
-				vim.fn.CocActionAsync("doHover")
-			else
-				vim.fn.feedkeys("K", "in")
-			end
-		end
-		vim.keymap.set("n", "K", ShowDocumentation, { noremap = true, silent = true })
-		if vim.fn.has("nvim-0.4.0") or vim.fn.has("patch-8.2.0750") then
-			vim.keymap.set("n", "<a-j>", "coc#float#has_scroll() ? coc#float#scroll(1) : '<a-j>'",
-				{ noremap = true, silent = true, expr = true, nowait = true })
-			vim.keymap.set("n", "<a-k>", "coc#float#has_scroll() ? coc#float#scroll(0) : '<a-k>'",
-				{ noremap = true, silent = true, expr = true, nowait = true })
-		end
-		-- set statusline+=%=%{coc#status()}%{get(b:,'coc_current_function','')}
-		vim.o.statusline = "%f" ..
-		vim.o.statusline .. "%=%{coc#status()}%{get(b:,'coc_current_function','')} %{strftime('%H:%M:%S')}"
+        vim.keymap.set("n", "<c-j>", "<plug>(coc-diagnostic-next)", { noremap = true, silent = true })
+        vim.keymap.set("n", "<c-k>", "<plug>(coc-diagnostic-prev)", { noremap = true, silent = true })
+        vim.keymap.set("n", "gt", "<plug>(coc-type-definition)", { noremap = true, silent = true })
+        vim.keymap.set("n", "gd", "<plug>(coc-definition)", { noremap = true, silent = true })
+        vim.keymap.set("i", "<c-space>", "coc#refresh()", { noremap = true, silent = true, expr = true })
+        vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", { noremap = true, silent = true })
+        vim.keymap.set("n", "gr", "<Plug>(coc-references)", { noremap = true, silent = true })
+        vim.keymap.set({ "i", "n" }, "<c-c>", function()
+            if vim.fn["coc#pum#visible"]() == 1 then
+                return vim.fn["coc#pum#cancel"]()
+            elseif vim.fn["coc#float#has_float"]() == 1 then
+                return vim.fn["coc#float#close_all"]()
+            else
+                return "<c-c>"
+            end
+        end, { noremap = true, silent = true, expr = true })
+        local function ShowDocumentation()
+            if vim.fn.CocAction("hasProvider", "hover") then
+                vim.fn.CocActionAsync("doHover")
+            else
+                vim.fn.feedkeys("K", "in")
+            end
+        end
+        vim.keymap.set("n", "K", ShowDocumentation, { noremap = true, silent = true })
+        if vim.fn.has("nvim-0.4.0") or vim.fn.has("patch-8.2.0750") then
+            vim.keymap.set("n", "<a-j>", "coc#float#has_scroll() ? coc#float#scroll(1) : '<a-j>'",
+                { noremap = true, silent = true, expr = true, nowait = true })
+            vim.keymap.set("n", "<a-k>", "coc#float#has_scroll() ? coc#float#scroll(0) : '<a-k>'",
+                { noremap = true, silent = true, expr = true, nowait = true })
+        end
+        -- set statusline+=%=%{coc#status()}%{get(b:,'coc_current_function','')}
+        vim.o.statusline = "%f" ..
+            vim.o.statusline .. "%=%{coc#status()}%{get(b:,'coc_current_function','')} %{strftime('%H:%M:%S')}"
 
-		vim.cmd [[
+        vim.cmd [[
 			inoremap <silent><expr> <TAB>
 			\ coc#pum#visible() ? coc#pum#next(1) :
 			\ CheckBackspace() ? "\<Tab>" :
@@ -68,6 +68,6 @@ return {
 			command! -bar -nargs=0 Snippets :CocCommand snippets.openSnippetFiles
 
 			]]
-	end
+    end
 
 }
--- nvim/.config/nvim/lua/plugins/codegpt.lua
+++ nvim/.config/nvim/lua/plugins/codegpt.lua
@@ -1,10 +1,19 @@
 return {
-	"dpayne/CodeGPT.nvim",
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'MunifTanjim/nui.nvim',
-	},
-	config = function()
-		require("codegpt.config")
-	end
+    "dpayne/CodeGPT.nvim",
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+    },
+    config = function()
+        -- vim.g["codegpt_global_commands_defaults"] = {
+        --     model = "gpt-4o",
+        --     -- max_tokens = 4096,
+        --     -- temperature = 0.4,
+        --     -- extra_parms = { -- optional list of extra parameters to send to the API
+        --     --     presence_penalty = 1,
+        --     --     frequency_penalty= 1
+        --     -- }
+        -- }
+        require("codegpt.config")
+    end
 }

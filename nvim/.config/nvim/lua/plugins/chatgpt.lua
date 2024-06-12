40f080181223978f8112629e2c723a125b3f9120
nikos efthias <nikos@mugsoft.io>
2024-06-12 03:33:47 +0300

    updates

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -1,13 +1,29 @@
 return {
-	"jackMort/ChatGPT.nvim",
-	event = "VeryLazy",
-	config = function()
-		require("chatgpt").setup()
-	end,
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"nvim-lua/plenary.nvim",
-		"folke/trouble.nvim",
-		"nvim-telescope/telescope.nvim"
-	}
+    "jackMort/ChatGPT.nvim",
+    lazy = true,
+    config = function()
+        require("chatgpt").setup {
+            chat = {
+                keymaps = {
+                    toggle_help = "<a-h>"
+                },
+                openai_params = {
+                    model = "gpt-4o"
+                },
+            },
+            popup_input = {
+                submit = "<Enter>",
+                submit_n = "<C-Enter>",
+            }
+        }
+    end,
+    keys = {
+        { "<space>g", ":ChatGPT<cr>" }
+    },
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim"
+    }
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,46 +1,45 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		lazy = true,
-		keys = { "<C-space>" },
-		cmd = { 'Telescope' },
-		config = function()
-			local telescope = require('telescope')
-			vim.cmd "cnoreabbrev copen Telescope quickfix"
-			telescope.setup {
-				defaults = {
-					mappings = {
-						i = {
-							["<C-j>"] = "move_selection_next",
-							["<C-k>"] = "move_selection_previous",
-							["<c-d>"] = "delete_buffer",
-							["<C-\\>"] = "which_key",
-							["<c-a>"] = "toggle_all",
-							["<c-]>"] = "send_selected_to_qflist",
-						},
-					},
-				},
-				pickers = {
-					find_files = {
-						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-					},
-				},
-			}
-			local builtins = require('telescope.builtin')
-			vim.keymap.set('n', '<space>f', builtins.find_files)
-			vim.keymap.set('n', '<space>g', builtins.git_files)
-			vim.keymap.set('n', '<space>b', builtins.buffers)
-			vim.keymap.set('n', '<space>t', builtins.tags)
-			vim.keymap.set('n', '<space>r', builtins.registers)
-			vim.keymap.set('n', '<space>h', builtins.grep_string)
-			vim.keymap.set('n', '<space>s', builtins.live_grep)
-			-- vim.keymap.set('i', '<C-space>s', builtins.grep_string)
-			--vim.keymap.set('n', '<a-d>', builtins.diagnostics)
-			--vim.keymap.set('n', '<c-space>s', builtins.lsp_workspace_symbols)
-			--vim.keymap.set('n', '<c-space>r', builtins.lsp_references)
-		end,
-	}
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        lazy = true,
+        keys = { "<C-space>" },
+        cmd = { 'Telescope' },
+        config = function()
+            local telescope = require('telescope')
+            vim.cmd "cnoreabbrev copen Telescope quickfix"
+            telescope.setup {
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = "move_selection_next",
+                            ["<C-k>"] = "move_selection_previous",
+                            ["<c-d>"] = "delete_buffer",
+                            ["<C-\\>"] = "which_key",
+                            ["<c-a>"] = "toggle_all",
+                            ["<c-]>"] = "send_selected_to_qflist",
+                        },
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                        find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                    },
+                },
+            }
+            local builtins = require('telescope.builtin')
+            vim.keymap.set('n', '<space>f', builtins.find_files)
+            vim.keymap.set('n', '<space>b', builtins.buffers)
+            vim.keymap.set('n', '<space>t', builtins.tags)
+            vim.keymap.set('n', '<space>r', builtins.registers)
+            vim.keymap.set('n', '<space>h', builtins.grep_string)
+            vim.keymap.set('n', '<space>s', builtins.live_grep)
+            -- vim.keymap.set('i', '<C-space>s', builtins.grep_string)
+            --vim.keymap.set('n', '<a-d>', builtins.diagnostics)
+            --vim.keymap.set('n', '<c-space>s', builtins.lsp_workspace_symbols)
+            --vim.keymap.set('n', '<c-space>r', builtins.lsp_references)
+        end,
+    }
 }

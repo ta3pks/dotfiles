d13e238d86171baf300c679f299e08a3f6e5d96a
nikos efthias <nikos@mugsoft.io>
2025-03-29 01:31:19 +0300

    various updates

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -4,21 +4,54 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
     },
+    keys = {
+        { "<leader><space>a", "<cmd>CodeCompanionChat toggle<cr>", mode = "n" },
+        { "<leader><space>i", "<cmd>CodeCompanion<cr>",            mode = "n" },
+        { "<leader><space>i", ":'<,'>CodeCompanion<cr>",           mode = "v" },
+    },
     config = function()
         require "codecompanion".setup {
+            adapters = {
+                opts = {
+                    -- allow_insecure = true,
+                    -- proxy = "socks5://192.168.1.64:6080",
+                },
+                anthropic = function()
+                    return require("codecompanion.adapters").extend("anthropic", {
+                        proxy = "socks5://localhost:6080",
+                        env = {
+                        },
+                    })
+                end,
+            },
             strategies = {
                 chat = {
                     adapter = "ollama",
+                    -- adapter = "anthropic",
                 },
                 inline = {
                     adapter = "ollama",
+                    -- adapter = "anthropic",
                 },
             },
             adapter = function()
+                require "codecompanion.adapters".extend("anthropic", {
+                    env = {
+                    },
+                    schema = {
+                        model = {
+                            default = "claude-3.7-sonnet",
+                        }
+                    },
+                }
+                )
                 require "codecompanion.adapters".extend("ollama", {
                     schema = {
                         model = {
-                            default = "qwen2.5-coder:14b"
+                            -- default = 'victornitu/rust-coder'
+                            -- default = "qwen2.5-coder:32b"
+                            -- default = "deepseek-coder-v2"
+                            default = 'codeastral'
                         }
                     }
                 })

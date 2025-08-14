8dbe79e981cadc8e224f0c2e08183d086f5ba557
nikos efthias <nikos@mugsoft.io>
2025-08-14 18:53:07 +0300

    updates

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,8 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- on buffer focus refresh contents to chech if externally edited
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = "*",
+  command = "silent! checktime",
+})

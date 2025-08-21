5717982a1f4572781e27083453f19f955c5d4ddc
nikos efthias <nikos@mugsoft.io>
2025-08-21 21:34:38 +0300

    rust remote testing

---
+++ local_bin/.local/bin/remote_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+__binary_path=$(cargo test --no-run --lib 2>&1 | sed -n 's/Executable .* (\(.*\))/\1/p')
+rsync -vahurz --progress $__binary_path dedecta:/tmp/test
+cmd="ssh dedecta -T /tmp/test -- --no-capture $@"
+echo "running $cmd"
+$cmd

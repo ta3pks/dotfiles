4d5ba262dc30872a6a05f68aa9d628f7402ad3b6
nikos efthias <nikos@mugsoft.io>
2025-08-21 21:45:13 +0300

    fix remote test script

--- local_bin/.local/bin/remote_test.sh
+++ local_bin/.local/bin/remote_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 __binary_path=$(cargo test --no-run --lib 2>&1 | sed -n 's/Executable .* (\(.*\))/\1/p')
 rsync -vahurz --progress $__binary_path REDACTED_PROJECT:/tmp/test
-cmd="ssh REDACTED_PROJECT -T /tmp/test -- --color=always --nocapture $@"
+cmd="ssh REDACTED_PROJECT -T /tmp/test --nocapture $@"
 echo "running $cmd"
 $cmd
